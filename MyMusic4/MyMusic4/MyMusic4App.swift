145f4e8920844b2ab02425bb9af2faff0e218d60
kakutagawa <kaktgw0824@gmail.com>
2023-11-13 08:44:27 +0900

    MyMusic4 OK

---
+++ MyMusic4/MyMusic4/MyMusic4App.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusic4App.swift
+//  MyMusic4
+//
+//  Created by 芥川浩平 on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct MyMusic4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyMusic4/MyMusic4/SoundPlayer.swift
@@ -0,0 +1,43 @@
+//
+//  SoundPlayer.swift
+//  MyMusic4
+//
+//  Created by 芥川浩平 on 2023/11/13.
+//
+
+import UIKit
+import AVFoundation
+
+final class SoundPlayer: NSObject {
+
+    private let cymbalData = NSDataAsset(name: "cymbalSound")?.data
+    private var cymbalPlayer: AVAudioPlayer?
+
+    private let guitarData = NSDataAsset(name: "guitarSound")?.data
+    private var guitarPlayer: AVAudioPlayer?
+
+    func cymbalPlay() {
+        do {
+            if let cymbalData {
+                cymbalPlayer = try AVAudioPlayer(data: cymbalData)
+            }
+            cymbalPlayer?.play()
+        } catch {
+            print("シンバルでエラーが発生しました")
+        }
+
+    }
+
+    func guitarPlay() {
+        do {
+            if let guitarData {
+                guitarPlayer = try AVAudioPlayer(data: guitarData)
+            }
+            guitarPlayer?.play()
+        } catch {
+            print("ギターでエラーが発生しました")
+        }
+
+    }
+
+}

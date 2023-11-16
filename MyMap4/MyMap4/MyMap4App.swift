146dddf3d98f59f3efde61f2e5b8b8378a449890
kakutagawa <kaktgw0824@gmail.com>
2023-11-17 08:32:27 +0900

    MyMap4 OK

---
+++ MyMap4/MyMap4/MyMap4App.swift
@@ -0,0 +1,17 @@
+//
+//  MyMap4App.swift
+//  MyMap4
+//
+//  Created by 芥川浩平 on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct MyMap4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

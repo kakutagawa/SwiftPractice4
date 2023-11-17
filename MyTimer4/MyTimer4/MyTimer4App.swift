6d38edc1eba8726fc317bd2644024b05ee0890db
kakutagawa <kaktgw0824@gmail.com>
2023-11-17 23:20:34 +0900

    MyTimer4 OK

---
+++ MyTimer4/MyTimer4/MyTimer4App.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimer4App.swift
+//  MyTimer4
+//
+//  Created by 芥川浩平 on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct MyTimer4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

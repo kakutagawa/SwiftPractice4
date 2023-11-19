3d26796fd9f971fa4cc57222d49aa4c952426783
kakutagawa <kaktgw0824@gmail.com>
2023-11-20 00:29:43 +0900

    MyCamera4 作成中

---
+++ MyCamera4/MyCamera4/MyCamera4App.swift
@@ -0,0 +1,17 @@
+//
+//  MyCamera4App.swift
+//  MyCamera4
+//
+//  Created by 芥川浩平 on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct MyCamera4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
